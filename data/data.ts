17d96cbea90051e0401417397c6cf8a2b9a21915
Kisora Thomas <kisora.thomas@gmail.com>
2024-09-25 10:35:08 -0600

    Update image paths for product cards

--- data/data.ts
+++ data/data.ts
@@ -3,56 +3,56 @@ const data = {
 
     products:[
         {
-            image:'/src/assets/images/rose1.jpg',
+            image:'./src/assets/images/rose1.jpg',
             name:'ROSE',
             price:6,
             priceId:'price_1PzoGP06xeklz4Gn7wd5DB4w',
             description:'Our Rose Love Card features a delicate, pressed rose, a timeless symbol of passion and romance. Roses have been cherished for centuries, dating back to ancient Greece and Rome. Our cards are handcrafted with care using recycled paper and organic flowers, making them eco-friendly and sustainable.'
         },
         {
-            image:'/src/assets/images/daisy1.jpg',
+            image:'./src/assets/images/daisy1.jpg',
             name:'DAISY',
             price:6,
             priceId: 'price_1Q0SO606xeklz4Gnu0ZfqjyG',
             description:'Our Daisy Innocence Card captures the cheerful spirit of a daisy. Daisies are a common wildflower found in meadows and fields around the world. This card is handcrafted with recycled paper and locally pressed daisies, supporting sustainable practices and preserving natural beauty.'
         },
         {
-            image:'/src/assets/images/flower6.jpg',
+            image:'./src/assets/images/flower6.jpg',
             name:'LAVENDER',
             price:6,
             priceId: 'price_1Q0SOH06xeklz4GnOqCwt6KT',
             description:'Our Lavender Serenity Card offers a calming and peaceful atmosphere. Lavender is a fragrant herb with a long history of use in aromatherapy and medicine. Our cards are handcrafted with recycled paper and organically grown lavender, promoting relaxation and well-being.'
         },
         {
-            image:'/src/assets/images/flower9.jpg',
+            image:'./src/assets/images/flower9.jpg',
             name:'IRIS',
             price:6,
             priceId: 'price_1Q0SOr06xeklz4GnOyeFmJcS',
             description:'Our Iris Hope Card offers a message of encouragement and inspiration. Irises are native to Europe and Asia and have been cultivated for centuries. Our cards are handcrafted with recycled paper and pressed irises, supporting sustainable practices and preserving natural beauty.'
         },
         {
-            image:'/src/assets/images/lily1.jpg',
+            image:'./src/assets/images/lily1.jpg',
             name:'LILY',
             price:6,
             priceId: 'price_1Q0SP606xeklz4GnHMkD1VmP',
             description:'Our Lily Purity Card is a symbol of innocence and rebirth. Perfect for baby showers, baptisms, or expressing your hopes for a brighter future. Our cards are handcrafted with recycled paper and pressed lilies, ensuring a beautiful and eco-friendly gift.'
         },
         {
-            image:'/src/assets/images/flower8.jpg',
+            image:'./src/assets/images/flower8.jpg',
             name:'CARNATION',
             price:6,
             priceId: 'price_1Q0SPj06xeklz4GnhXsYg6Di',
             description:'Our Pink Carnation Gratitude Card conveys a heartfelt message of thanks. Carnations are native to the Mediterranean region and have been cultivated for centuries. Our cards are handcrafted with recycled paper and organically grown carnations, promoting sustainability and environmental responsibility.'
         },
         {
-            image:'/src/assets/images/flower5.jpg',
+            image:'./src/assets/images/flower5.jpg',
             name:'POPPY',
             price:6,
             priceId: 'price_1Q0SPx06xeklz4Gnc3an2fIq',
             description:'Our Poppy Remembrance Card is a tribute to cherished memories. Poppies are associated with World War I and are often used to commemorate fallen soldiers. This card is perfect for expressing sympathy, honoring a loved one, or commemorating a special occasion.'
         },
         {
-            image:'/src/assets/images/sunflower1.jpg',
+            image:'./src/assets/images/sunflower1.jpg',
             name:'SUNFLOWER',
             price:6,
             priceId: 'price_1Q0SQA06xeklz4GnSRypnd8Z',
